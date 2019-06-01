da0056c1f116a817042f3f882d5e58a025d907e4
Nikitesh <nikiteshb@gmail.com>
2019-06-01 10:44:55 +0530

    nikitesh first project

---
+++ first/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeroComponent } from './homepage/hero/hero.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HeroComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ first/src/app/homepage/hero/hero.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hero',
+  templateUrl: './hero.component.html',
+  styleUrls: ['./hero.component.scss']
+})
+export class HeroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  title = 'Nikitesh.com';
+  name="Nikitesh Bhadade";
+  subtitle = 'yatra tatra sarwatra';
+}
