c5f62b2b6608c68de63d6dbabf149ceb250cdacb
Nikitesh <nikiteshb@gmail.com>
2020-05-05 00:03:31 +0530

    validations added

--- nikiteshang/app/src/app/shared/contactform/contactform.component.ts
+++ nikiteshang/app/src/app/shared/contactform/contactform.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 import { RestApiService } from 'src/app/shared/rest-api.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { finalize } from 'rxjs/operators';
@@ -12,27 +12,38 @@ import { finalize } from 'rxjs/operators';
 export class ContactformComponent implements OnInit {
   successMsg: boolean = false;
   public isSubmitting = false
+  contactForm: FormGroup;
 
   constructor(private fb: FormBuilder,
     public restApi: RestApiService, 
     public route: ActivatedRoute,
     public router: Router
     ) { }
-  contactForm = this.fb.group({
-    firstName: ['', Validators.required],
-    lastName: ['', Validators.required],
-    emailId: ['', Validators.required],
-    contactNo: ['', Validators.required],
-    message: ['']    
-  });
+    
+  
 
-  ngOnInit(): void {
+  ngOnInit(){
+    this.contactForm = this.fb.group({
+      firstName: ['', [Validators.required,Validators.minLength(2)]],
+      lastName: ['', [Validators.required,Validators.minLength(2)]],
+      emailId: ['', [Validators.required, Validators.email]],
+      contactNo: ['', [Validators.required,Validators.minLength(10)]],
+      message: ['']    
+    });
+  }
+  get contactFormControl() {
+    return this.contactForm.controls;
   }
   
   onSubmit() {
     // TODO: Use EventEmitter with form value
     this.isSubmitting = true;
     console.warn(this.contactForm.value);
+    
+    if (this.contactForm.invalid) {
+      return;
+    }
+
     this.restApi.createLead(this.contactForm.value).pipe(
       finalize(() => this.isSubmitting = false),
     ).subscribe((data: {}) => {
