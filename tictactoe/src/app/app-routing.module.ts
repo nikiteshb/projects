940e9afb0663d80ffba03ff1e1b5aa73a41c44cd
Nikitesh <nikiteshb@gmail.com>
2020-05-24 20:03:26 +0530

    mnp

--- tictactoe/src/app/app-routing.module.ts
+++ tictactoe/src/app/app-routing.module.ts
@@ -3,14 +3,6 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  // { path: '',   redirectTo: '/', pathMatch: 'full' },
-  // { path: '', component: HomepageComponent },
-  // { path: 'home/#/home', component: HomepageComponent },
-  // { path: 'add-user', component: AddUserComponent },
-  // { path: 'edit-user/:id', component: EditUserComponent },
-  // { path: 'list-user', component: ListUserComponent },
-  // { path: '**', redirectTo: '/404' },
-  // {path: '404', component: NotfoundComponent},
 ];
 
 @NgModule({
